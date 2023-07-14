224eacb7e399987807c58e9779ce170d6b272cbb
Christian Laguna <elprogramador.nogenio@gmail.com>
2023-07-14 11:09:28 -0500

    Implementación de la galeria de imagen

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbDropdownModule, NgbModule, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
-
-
+import { GalleryModule, GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
+import { LightboxModule } from 'ng-gallery/lightbox';
 
 @NgModule({
   declarations: [],
@@ -12,6 +12,8 @@ import { ToastrModule } from 'ngx-toastr';
     NgbModule,
     NgbDropdownModule,
     NgbNavModule,
+    GalleryModule,
+    LightboxModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     })
@@ -20,7 +22,18 @@ import { ToastrModule } from 'ngx-toastr';
     NgbModule,
     NgbDropdownModule,
     NgbNavModule,
-    ToastrModule
+    ToastrModule,
+    GalleryModule,
+    LightboxModule
+  ],
+  providers: [
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'cover'
+      } as GalleryConfig
+    }
   ]
 })
 export class SharedModule { }
--- client/src/app/members/member-details/member-details.component.ts
+++ client/src/app/members/member-details/member-details.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from 'src/app/_models/member';
 import { MembersService } from 'src/app/_services/members.service';
+import { GalleryComponent, ImageItem, GalleryItem, Gallery, ImageSize, ThumbnailsPosition } from 'ng-gallery';
+import { Lightbox } from 'ng-gallery/lightbox';
 
 @Component({
   selector: 'app-member-details',
@@ -11,9 +13,14 @@ import { MembersService } from 'src/app/_services/members.service';
 export class MemberDetailsComponent implements OnInit {
 
   member: Member | undefined;
+  photos: GalleryItem[] = [];
   active = 1;
 
-  constructor(private memberService: MembersService, private route: ActivatedRoute) {}
+  constructor(
+    private memberService: MembersService,
+    private route: ActivatedRoute,
+    public gallery: Gallery,
+    public lightbox: Lightbox) {}
 
   ngOnInit(): void {
     this.loadMember();
@@ -24,8 +31,25 @@ export class MemberDetailsComponent implements OnInit {
     const username = this.route.snapshot.paramMap.get('username');
     if(!username) return;
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {
+        this.member = member;
+        this.getImages();
+      }
     });
+  }
+
+  getImages() {
+    if(!this.member) return;
+    this.photos = this.member
+      .photos.map( photo => new ImageItem(
+        { src: photo.url, thumb: photo.url, alt: this.member?.knownAs }
+        ));
+    const lightboxRef = this.gallery.ref('lightbox');
+    lightboxRef.setConfig({
+      imageSize: ImageSize.Cover,
+      thumbPosition: ThumbnailsPosition.Top,
+    });
+    lightboxRef.load(this.photos);
   }
 
 }
