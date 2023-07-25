263581e17f35e75468c20914ef99234706ff34c5
Christian Laguna <elprogramador.nogenio@gmail.com>
2023-07-25 14:59:47 -0500

    entidad relacion de muchos a muchos

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -27,5 +27,8 @@ namespace API.Entities
             return DateOfBirth.CalculateAge();
         }*/
 
+        public List<UserLike> LikedByUsers { get; set; }
+        public List<UserLike> LikedUsers { get; set; }
+
     }
 }
