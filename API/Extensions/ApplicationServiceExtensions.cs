1a02369bcfd5379f0b8d9760dc74702d583beb78
Christian Laguna <elprogramador.nogenio@gmail.com>
2023-07-31 17:04:37 -0500

    Empezamos la seccion 15

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -24,6 +24,7 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
             services.AddScoped<IphotoService, PhotoService>();
             services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
             services.AddScoped<LogUserActivity>();
             return services;
         }
