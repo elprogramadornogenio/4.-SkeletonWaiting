bf6fe90c3d5c122d538e87be9fe11a5ad5b5fdca
Christian Laguna <elprogramador.nogenio@gmail.com>
2023-08-16 16:54:16 -0500

    Comentarios de Paginación

--- API/Helpers/PagedList.cs
+++ API/Helpers/PagedList.cs
@@ -4,31 +4,59 @@ namespace API.Helpers
 {
     public class PagedList<T>: List<T>
     {
-        public PagedList(IEnumerable<T> items, 
-            int count, int pageNumber, int pageSize)
+        public PagedList(
+            IEnumerable<T> items, // secuencia de tipo generico (recibe cualquier tipo de clase)
+            int count, // recuento total de elementos
+            int pageNumber, // numero actual de la pagina
+            int pageSize // tamaño de datos en una página
+            )
         {
-            CurrentPage = pageNumber;
-            TotalPages = (int) Math.Ceiling(count/(double) pageSize);
-            PageSize = pageSize;
-            TotalCount = count;
-            AddRange(items);
+            CurrentPage = pageNumber; // guarda el numero actual de la página
+            TotalPages = (int) Math.Ceiling(count/(double) pageSize); // Total de paginas
+            /*
+                Divide count/ pageSize es decir count(total de elementos)/ pageSize (tamaño de la pagina)
+                Math.Ceiling redondea el resultado anterior hacia arriba y lo castea a entero
+            */
+            PageSize = pageSize; // numero total de elementos en una pagina
+            TotalCount = count; // total de elementos
+            AddRange(items); // agrega los elementos de la colección a una lista existente
         }
 
-        public int CurrentPage { get; set; }
-        public int TotalPages { get; set; }
-        public int PageSize { get; set; }
-        public int TotalCount { get; set; }
+        public int CurrentPage { get; set; } // guarda la pagina actual
+        public int TotalPages { get; set; } // guarda el total de las paginas calculadas
+        public int PageSize { get; set; } // guarda el numero de elementos por pagina
+        public int TotalCount { get; set; } // total de elementos
 
         public static async Task<PagedList<T>> CreateAsync(
-            IQueryable<T> source, 
-            int pageNumber, 
-            int pageSize) 
+            IQueryable<T> source, // resultado de una consulta de bases de datos
+            int pageNumber, // número de paginas 
+            int pageSize // numero de elementos por pagina
+            ) 
         {
-            var count = await source.CountAsync();
+            var count = await source.CountAsync(); // obtiene el total de elementos del
+            // resultado de la consulta IQueryable
             var items = await source.Skip((pageNumber -1)* pageSize)
                 .Take(pageSize).ToListAsync();
+                /*
+                    source.Skip es el numero de elementos a omitir
+                    con la formula (pageNumber-1)* pageSize 
+                    por ejemplo
+                    en estos momentos me encuentro quiero los elementos de la página 3
+                    debo omitir los elementos de las paginas 1 y 2 
+                    es por esta razón (pageNumber-1)
+                    ahora se toma el numero de elementos de la página y se multiplica por
+                    (pageNumber-1) digamos que cada pagina tiene 5 elementos
+                    por lo tanto (3-1)*5 = (2)*5 = 10 es decir que 10 elementos deben ser
+                    omitidos
+                    con take se toma el numero de elementos especificados despues de los
+                    elementos omitidos
+                    despues el método ToListAsync consulta la base de datos y lista los
+                    elementos
+
+                */ 
             
-            return new PagedList<T>(items, count, pageNumber, pageSize);
+            return new PagedList<T>(items, count, pageNumber, pageSize); // se insertan
+            // los elementos al constructor de la misma clase.
             
         }
     }
--- API/Service/PhotoService.cs
+++ API/Service/PhotoService.cs
@@ -9,39 +9,53 @@ namespace API.Service
     public class PhotoService : IphotoService
     {
         private readonly Cloudinary _cloudinary;
-        public PhotoService(IOptions<CloudinarySettings> config)
+        public PhotoService(IOptions<CloudinarySettings> config) 
+        // este parametro inyecta las configuraciones de cloudinary que se encuentran
+        // en appsettings.json
         {
-            var acc = new Account(
-                config.Value.CloudName,
+            var acc = new Account( // se recibe todas las configuraciones de cloudinary
+                config.Value.CloudName, 
                 config.Value.ApiKey,
                 config.Value.ApiSecret
             );
-            _cloudinary = new Cloudinary(acc);
+            _cloudinary = new Cloudinary(acc); // el objeto cloundary recibe la configuración
+            // es decir CloudName, ApiKey, ApiSecret
         }
-        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file) // representa el archivo
         {
-            var uploadResult = new ImageUploadResult();
+            var uploadResult = new ImageUploadResult(); // contiene los parametros para subir
+            // imagenes en cloudinary, sea tamaño, transformaciones, posición de la imagen
             
-            if(file.Length > 0)
+            if(file.Length > 0) //  si existe el archivo
             {
-                using var stream = file.OpenReadStream();
-                var uploadParams = new ImageUploadParams
+                using var stream = file.OpenReadStream(); // establece un punto de lectura para
+                // acceder a los archivos de la interfaz IFormFile
+                var uploadParams = new ImageUploadParams 
                 {
-                    File = new FileDescription(file.FileName, stream),
-                    Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face"),
-                    Folder = "da-net7"
+                    File = new FileDescription(file.FileName, stream), // proporciona el nombre
+                    // del archivo y mediante la variable stream el contenido
+                    Transformation = new Transformation().Height(500)
+                    .Width(500).Crop("fill").Gravity("face"),
+                    // Height(Alto = 500px) Width(Ancho= 500px)
+                    // Crop("fill") recorta("fill") recorta la imagen a 500px sin distorcionarla
+                    // Gravity("face") indica que la transformación debe ser centrada en la cara
+                    Folder = "da-net7" // se establece la carpeta donde se va a guardar las imagenes
                 };
-                uploadResult = await _cloudinary.UploadAsync(uploadParams);
+                uploadResult = await _cloudinary.UploadAsync(uploadParams); 
+                // se sube el archivo a cloudinary
             }
 
-            return uploadResult;
+            return uploadResult; // devuelve el resultado
         }
 
         public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        // publicId representa el identificador publico de la imagen en cloudinary
         {
-            var deleteParams = new DeletionParams(publicId);
+            var deleteParams = new DeletionParams(publicId); 
+            // crea un DeletionParams con el identificador publico de la imagen en cloudinary
 
-            return await _cloudinary.DestroyAsync(deleteParams);
+            return await _cloudinary.DestroyAsync(deleteParams); // elimina la imagen de acuerdo
+            // con los parametros de DeletionParams.
         }
     }
 }
